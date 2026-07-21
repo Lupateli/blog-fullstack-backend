7e556eb9813763728c98172981d33150978e0a07
Lupateli <glupateli@gmail.com>
2026-07-21 12:13:13 -0300

    docs: add swagger api documentation

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import postRoutes from "./routes/post.routes";
 import path from "path";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger";
 
 
 const app = express();
@@ -16,5 +18,6 @@ app.use(
   "/uploads",
   express.static(path.resolve("uploads"))
 );
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
---
+++ src/config/swegger.ts
@@ -0,0 +1,27 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+export const swaggerSpec = swaggerJsdoc({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Blog API",
+      version: "1.0.0",
+      description: "API do desafio fullstack da Mind Group",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+});
